7ab67be2f09a824d67bcbc7b4e7dbfa7bafcf248
luxianping <654894017@qq.com>
2024-11-09 14:02:11 +0800

    1.移除撮合引擎实例，到单独工程

--- cqrs-sample/cqrs-sample-generic-test/src/main/java/com/damon/cqrs/sample/trade_matching/domain/cmd/CallAuctionCmd.java
+++
@@ -1,9 +0,0 @@
-package com.damon.cqrs.sample.trade_matching.domain.cmd;
-
-import com.damon.cqrs.domain.Command;
-
-public class CallAuctionCmd extends Command {
-    public CallAuctionCmd(Long commandId, Long aggregateId) {
-        super(commandId, aggregateId);
-    }
-}
